bcbc33e44b80643550217bd1631c0c1bda728826
Bilal El Maftouhi <maftouhi.bilal@yahoo.com>
2023-07-10 19:51:58 +0100

    solve

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
-board = [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    [9, 0, 1, 2, 3, 4, 5, 6, 7],
-    [8, 9, 0, 1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 0, 1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9, 0, 1, 2, 3],
-    [4, 5, 6, 7, 8, 9, 0, 1, 2],
-    [3, 4, 5, 6, 7, 8, 9, 0, 1],
-    [2, 3, 4, 5, 6, 7, 8, 9, 0]
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
 
@@ -29,6 +29,7 @@ def find_empty(baord):
         for j in range(len(baord[1])):
             if baord[i][j] == 0:
                 return i, j
+    return None
 
 
 def validation(board, num, pos):
@@ -52,3 +53,22 @@ def validation(board, num, pos):
                 return False
 
     return True  # No Duplicate
+
+
+def solve(board):
+    if not find_empty(board):
+        return True
+    else:
+        x, y = find_empty(board)
+        for i in range(1, 10):
+            if validation(board, i, (x, y)):
+                board[x][y] = i
+
+                if solve(board):
+                    return True
+                board[x][y] = 0
+    return False
+
+
+solve(sudoku_board)
+print_board(sudoku_board)
